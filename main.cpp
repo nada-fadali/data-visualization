9a3eeb29c6d0c72d087308d90e7a1df098cd5237
Nada Fadali <nada.fadali@gmail.com>
2014-12-11 19:58:33 +0200

    merge into master

--- main.cpp
+++ main.cpp
@@ -1,8 +1,81 @@
-#include <GL/glut.h>
+#include <GL/glut.h>	// opengl
+#include <fstream>		// read txt file
+#include <string>		// string
+#include <iostream>		// cout
+#include <vector>		// vector
 
 using namespace std;
 
 
+///////////////////////////////
+//	CLASSES
+///////////////////////////////
+
+//=========> Node
+class Node {
+public:
+    string name;
+    int value;
+    Node* Parent;
+    vector<Node*> children;
+    Node();
+    Node(string,int,vector<Node*>);
+
+};
+
+Node::Node(){
+    name = "";
+    value = 0;  
+}
+
+Node::Node(string n , int v, vector<Node*> c) {
+    name = n;
+    value = v;
+    children = c;
+}
+
+//=========> HashNode
+class HashNode { 
+public:
+    Node key;
+    Node value;
+    HashNode(Node,Node);
+    HashNode();  
+};
+
+HashNode::HashNode(){ 
+}
+
+HashNode::HashNode(Node k, Node v) {
+    key = k;
+    value = v;
+}
+
+//=========> Graph
+class Graph {
+public:
+    Node R1;
+    Node R2;
+    Node parentR1;
+    Node parentR2;
+    HashNode relationtable;
+    Graph();
+    Graph(Node,Node,HashNode);
+};
+
+Graph::Graph(){
+}
+
+Graph::Graph(Node k, Node v , HashNode rt){
+    R1 = k;
+    R2 = v;
+    relationtable = rt;
+}
+
+
+//////////////////////////////
+//	OPENGL
+/////////////////////////////
 void init(void)
 {
 	glEnable(GL_DEPTH_TEST);
@@ -19,7 +92,6 @@ void handleResize(int w, int h)
 
 }
 
-
 void drawAxis()
 {
 	// x-axis
@@ -28,13 +100,13 @@ void drawAxis()
 		glVertex3f(1.0,0.0,0.0);
 		glVertex3f(-1.0,0.0,0.0);
 	glEnd();
-			// y-axis
+	// y-axis
 	glBegin(GL_LINES);
 		glColor3f(0.0,1.0,0.0);
 		glVertex3f(0.0,1.0,0.0);
 		glVertex3f(0.0,-1.0,0.0);
 	glEnd();
-		// z-axis
+	// z-axis
 	glBegin(GL_LINES);
 		glColor3f(0.0,0.0,1.0);
 		glVertex3f(0.0,0.0,-1.0);
@@ -60,9 +132,61 @@ void display(void)
 	glutSwapBuffers();
 }
 
-int main(int argc, char **argv)
-{  
 
+//////////////////////////////
+//	MAIN
+//////////////////////////////
+int main(int argc, char **argv)
+{
+	ifstream myfile ("inputs.txt");
+	if (myfile.is_open()){
+		string line;
+		while ( getline (myfile,line) ){
+			//split by ":" 
+			vector <string> lineAttr;
+			vector <string> roots;
+			vector <Node> children;
+			Node child;
+			//vector <Node> relations;
+  
+			int counter;
+
+	  		split( lineAttr, line, is_any_of( ":" ) );
+			while (!EOF){
+		  		for (int i=0 ; i< lineAttr.size(); i++){
+			
+					if (lineAttr[2]== "{") {
+					//roots[counter] = lineAttr[0];
+					counter++;
+					roots.push_back(lineAttr[0]);
+					//child(lineAttr[0],lineAttr[1],NULL) .. this should be for the next line .. needs some modifications
+					children.push_back(child);
+					Node node(lineAttr(0),lineAttr(1), children);
+				} 
+				else (lineAttr[2]== "."){
+					Node node(lineAttr(0),lineAttr(1), null);
+				}
+			
+				//else { //node
+					//create relation
+					//relations.push_back(lineAttr[2]);
+				//	Node node (lineAttr(0),lineAttr(1), null);
+				//}
+
+				if (lineAttr[0] == "}"){
+					counter--;
+					// create instance 
+			
+					roots.pop_back();
+				}
+		  	}	
+		}
+		myfile.close();
+    }
+    else cout << "Unable to open file";
+
+
+    // start opengl
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(1000, 600);
