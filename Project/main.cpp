ac84d4be39b02988655d30f6d28e06c2f1270653
Rawan Gamal <rawan.gmg@gmail.com>
2014-12-11 17:16:06 +0200

    graph but needs modifications

--- Project/main.cpp
+++ Project/main.cpp
@@ -21,10 +21,9 @@ class Node {
 public:
     string name;
     int value;
-    vector<Node*> relation;
     vector<Node*> children;
     Node();
-    Node(string,int,vector<Node*>,vector<Node*>);
+    Node(string,int,vector<Node*>);
 
 };
 
@@ -37,15 +36,105 @@ Node::Node(){
     
 }
 
-Node::Node(string n , int v,vector<Node*> r, vector<Node*> c) {
+Node::Node(string n , int v, vector<Node*> c) {
 
 
 
     name = n;
     value = v;
-    relation = r;
     children = c;
 
+
+
+
+}
+
+
+
+class HashNode {
+    
+public:
+    Node key;
+    Node value;
+    HashNode(Node,Node);
+    HashNode();
+    Node getKey()  {
+        return key;
+    }
+    
+    void setKey(Node key) {
+    
+        HashNode::key = key;
+    
+    }
+    
+    Node getValue() {
+        return value;
+    }
+    
+    void setValue(Node value) {
+        HashNode::value = value;
+    }
+  
+ 
+    
+};
+
+HashNode::HashNode(){
+   
+    
+    
+}
+
+HashNode::HashNode(Node k, Node v) {
+
+    key = k;
+    value = v;
+
+}
+
+
+class Graph {
+
+public:
+    
+    Node R1;
+    Node R2;
+    Node parentR1;
+    Node parentR2;
+    HashNode relationtable;
+    Graph();
+    Graph(Node,Node,HashNode);
+    
+
+    
+    Node getParentR1(){
+    
+    
+        return parentR1;
+    
+    }
+    
+    
+    
+    Node getParentR2(){
+    
+    
+        return parentR2;
+    
+    }
+};
+
+
+Graph::Graph(){
+
+}
+
+Graph::Graph(Node k, Node v , HashNode rt){
+
+    R1 = k;
+    R2 = v;
+    relationtable = rt;
 
 
 
@@ -68,7 +157,8 @@ int main(int argc, const char * argv[]) {
 	vector <string> lineAttr;
 	vector <string> roots;
 	vector <Node> children;
-	vector <Node> relations;
+	Node child;
+	//vector <Node> relations;
   
 	int counter;
 
@@ -80,22 +170,28 @@ int main(int argc, const char * argv[]) {
 				//roots[counter] = lineAttr[0];
 				counter++;
 				roots.push_back(lineAttr[0]);
-				Node node(lineAttr(0),lineAttr(1), children, relations);
+				//child(lineAttr[0],lineAttr[1],NULL) .. this should be for the next line .. needs some modifications
+				children.push_back(child);
+				Node node(lineAttr(0),lineAttr(1), children);
 			} 
-			else if (lineAttr[2]== "."){
-				Node node(lineAttr(0),lineAttr(1), null, null);
+			else (lineAttr[2]== "."){
+				Node node(lineAttr(0),lineAttr(1), null);
 			}
 			
-			else { //node
+			//else { //node
 
 				//create relation
-				relations.push_back(lineAttr[2]);
-				Node node (lineAttr(0),lineAttr(1), null, relations);
-			}
+				
+				//relations.push_back(lineAttr[2]);
+			//	Node node (lineAttr(0),lineAttr(1), null);
+			//}
 
 		if (lineAttr[0] == "}"){
 			counter--;
-			roots.pop_back(); 
+			// create instance 
+			
+			roots.pop_back();
+
 		}
 	
 		  }
